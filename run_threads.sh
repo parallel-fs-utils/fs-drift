7bc364116a3dbb0771dd54581f21534d7ec0bba5
Ben England <bengland@redhat.com>
2015-02-08 05:10:23 -0500

    initial commit

---
+++ run_threads.sh
@@ -0,0 +1,40 @@
+#!/bin/bash 
+#
+# run_threads.sh - run multiple processes with fsstress.py
+# edit test parameters in multi_thread_config.sh
+#
+
+. ./multi_thread_config.sh
+
+starting_gun=starting-gun.tmp
+stress_opts="-t $topdir -S $starting_gun -d $duration -f $files -s $size -r $recsz -l $levels -i $interval "
+if [ -n "$workload" ] ; then
+  stress_opts="$stress_opts -w $workload "
+fi
+cd `dirname $stress_pgm`
+rm -rf $logdir
+mkdir -p $logdir
+if [ -z "$threads" ] ; then
+  echo "usage: ./run_threads.sh thread-count"
+  exit 1
+fi
+if [ $threads -gt 0 ] ; then
+  for n in `seq 1 $threads` ; do 
+    eval "$stress_pgm $stress_opts > $logdir/`hostname`.thr$n.log 2>&1 &" 
+    pids="$pids $!"
+  done
+fi
+sleep 2
+if [ -z "$fss_multihost_test" ] ; then
+  touch $topdir/$starting_gun
+fi
+worst_status=0
+for p in $pids ; do
+  wait $p
+  s=$?
+  if [ $s != 0 ] ; then 
+    worst_status=$s
+    echo "pid $p status $s"
+  fi
+done
+exit $worst_status
