2ae3297f34f6f352f1d9b0c40d6726dff0b431ab
Samuel Petrovic <spetrovi@redhat.com>
2018-10-16 12:06:22 +0200

    Random file size is at least 1 byte
    Decision from Jun 2018 meeting
    
    Signed-off-by: Samuel Petrovic <spetrovi@redhat.com>

--- fsop.py
+++ fsop.py
@@ -171,7 +171,7 @@ def gen_random_fn(is_create=False):
 
 
 def random_file_size():
-    return random.randint(0, opts.max_file_size_kb * BYTES_PER_KB)
+    return random.randint(1, opts.max_file_size_kb * BYTES_PER_KB)
 
 
 def random_record_size():
