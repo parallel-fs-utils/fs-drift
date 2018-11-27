5a84e973c64b84d8d049a6096ff7dd0b974fd0a9
Ben England <bengland@redhat.com>
2018-11-27 16:37:06 -0500

    do informational logging to stdout, do debug logging to file

--- fsd_log.py
+++ fsd_log.py
@@ -1,14 +1,23 @@
+import os
 import logging
+
 # standardize use of logging module in fs-drift
 
 def start_log(prefix):
     log = logging.getLogger(prefix)
-    h = logging.StreamHandler()
     log_format = prefix + ' %(asctime)s - %(levelname)s - %(message)s'
     formatter = logging.Formatter(log_format)
-    h.setFormatter(formatter)
-    log.addHandler(h)
-    log.setLevel(logging.DEBUG)
-    return log
 
-    #with open('/tmp/weights.csv', 'w') as w_f:
+    h = logging.StreamHandler()
+    h.setFormatter(formatter)
+    h.setLevel(logging.INFO)
+    log.addHandler(h)
+
+    h2 = logging.FileHandler('/var/tmp/fsd.%s.log' % prefix)
+    h2.setFormatter(formatter)
+    h2.setLevel(logging.DEBUG)
+    if os.getenv('LOGLEVEL_DEBUG'):
+        h2.setLevel(logging.DEBUG)
+    log.addHandler(h2)
+
+    return log
