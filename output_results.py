ce3b7733634aedfe8b26a47b4bdd20c95ef979a9
Ben England <bengland@redhat.com>
2018-11-28 09:09:13 -0500

    oops

--- output_results.py
+++ output_results.py
@@ -5,6 +5,6 @@ def print_stats(start_time, total_errors, fsop_ctrs):
     print('')
     print('elapsed time: %9.1f' % (time.time() - start_time))
     print('%9u = total errors' % total_errors)
-    print(json.dumps(fsop_ctrs.json_dict(), indent=4)
+    print(json.dumps(fsop_ctrs.json_dict(), indent=4))
     sys.stdout.flush()
 
