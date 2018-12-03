622f20dd7560b93d66393e4d5f9a143b4480d24b
Ben England <bengland@redhat.com>
2018-12-03 16:49:36 -0500

    first reasonable attempt using code from smallfile
    has code for remote hosts, subprocesses, etc.

--- fs-drift.py
+++ fs-drift.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python2
 
-# fs-drift.py - user runs this module to generate workload
-# "-h" option generates online help
+# fs-drift.py - run a mixed, multi-thread, multi-host workload
+# on a POSIX filesystem
+
+# all the heavy lifting is done in "invocation" module,
+# how to run:
+#
+# ./fs-drift.py
+# 
+# how to get help:
+#
+# ./fs-drift.py -h
+#
+# documentation at https://github.com/parallel-fs-utils/fs-drift
+#
+
+'''
+Copyright 2015 -- Ben England
+Licensed under the Apache License at http://www.apache.org/licenses/LICENSE-2.0
+See Appendix on this page for instructions pertaining to license.
+'''
 
 import os
 import os.path
@@ -12,120 +30,242 @@ import event
 import errno
 import pickle
 
-import fsop
 import common
 from common import rq, OK, NOTOK
+from common import ensure_deleted, FsDriftException
 import opts
+import fsd_log
 import output_results
+import ssh_thread
+import sync_files
+import output_results
+import multi_thread_workload
+from sync_files import write_pickle, read_pickle
 
-# the main program
+def abort_test(prm):
+    multi_thread_workload.abort_test(prm.abort_path, remote_thread_list)
+    sys.exit(NOTOK)
 
+# run a multi-host test
 
-params = opts.parseopts()
-event.parse_weights(params)
-event.normalize_weights()
-total_errors = 0
-fsop.init_buf(params)
-if len(params.top_directory) < 6:
-    raise FsDriftException(
-            'top directory %s too short, may be system directory' % 
-            params.top_directory)
+def run_multi_host_workload(prm, log):
 
-try:
-    os.mkdir(params.network_shared_path)
-except os.error as e:
-    if e.errno != errno.EEXIST:
-        raise e
+    # construct list of ssh threads to invoke in parallel
 
-# save params in a place where remote workload generators can read them
+    sync_files.create_top_dirs(prm)
+    sync_files.write_pickle(prm.param_pickle_path, prm)
+    if os.getenv('PYPY'):
+        python_prog = os.getenv('PYPY')
+    elif sys.version.startswith('2'):
+        python_prog = 'python'
+    elif sys.version.startswith('3'):
+        python_prog = 'python3'
+    else:
+        raise Exception('unrecognized python version %s' % sys.version)
 
-with open(params.param_pickle_path, 'w') as pickle_f:
-    pickle.dump(params, pickle_f)
-    
-os.chdir(params.top_directory)
-sys.stdout.flush()
+    log.debug('python_prog = %s'%python_prog)
 
-op = 0
-rsptimes = {'read': [], 'random_read': [], 'create': [], 'random_write': [], 'append': [
-], 'link': [], 'delete': [], 'rename': [], 'truncate': [], 'hardlink': []}
-last_stat_time = time.time()
-last_drift_time = time.time()
-stop_file = params.stop_file_path
+    remote_thread_list = []
+    host_ct = len(prm.host_set)
+    for j in range(0, len(prm.host_set)):
+        remote_host = prm.host_set[j]
+        smf_remote_pgm = os.path.join(prm.remote_pgm_dir,
+                                      'fs-drift-remote.py')
+        this_remote_cmd = '%s %s --network-sync-dir %s ' \
+            % (python_prog, smf_remote_pgm, prm.network_shared_path)
 
-# we have to synchronize threads across multiple hosts somehow, we do this with a
-# file in a shared file system.
+        this_remote_cmd += ' --as-host %s' % remote_host
+        log.debug(this_remote_cmd)
+        remote_thread_list.append(ssh_thread.ssh_thread(remote_host,
+                                                        this_remote_cmd))
 
-#if params.starting_gun_file:
-#    while not os.access(params.starting_gun_file, os.R_OK):
-#        time.sleep(1)
-#time.sleep(2)  # give everyone else a chance to see that start-file is there
-start_time = time.time()
-event_count = 0
+    # start them, pacing starts so that we don't get ssh errors
 
-while True:
-    # if there is pause file present, do nothing
+    for t in remote_thread_list:
+        time.sleep(0.1)
+        t.start()
 
-    if os.path.isfile(params.pause_file):
-        time.sleep(5)
-        continue
+    # wait for hosts to arrive at starting gate
+    # if only one host, then no wait will occur
+    # as starting gate file is already present
+    # every second we resume scan from last host file not found
 
-    # every 1000 events, check for "stop file" that indicates test should end
+    exception_seen = None
+    abortfn = prm.abort_path
+    sec_delta = 0.5
+    # timeout if no host replies in next host_timeout seconds
+    host_timeout = 10
 
-    event_count += 1
-    if (event_count % 1000 == 0) and os.access(stop_file, os.R_OK):
-        break
-
-    # if using operation count to limit test
-
-    if params.opcount > 0:
-        if op >= params.opcount:
-            break
-        op += 1
-
-    # if using duration to limit test
-
-    if params.duration > 0:
-        elapsed = time.time() - start_time
-        if elapsed > params.duration:
-            break
-    x = event.gen_event()
-    (fn, name) = fsop.rq_map[x]
-    if common.verbosity & 0x1:
-        print()
-        print(x, name)
-    before = time.time()
-    before_drift = time.time()
-    curr_e_exists, curr_e_not_found = fsop.e_already_exists, fsop.e_file_not_found
+    hosts_ready = False  # set scope outside while loop
+    last_host_seen = -1
+    sec = 0.0
     try:
-        rc = fn(params)
-        after = time.time()
-        if curr_e_exists == fsop.e_already_exists and curr_e_not_found == fsop.e_file_not_found:
-            rsptimes[name].append((before - start_time, after - before))
+        while sec < host_timeout:
+            # HACK to force directory entry coherency for Gluster
+            #ndirlist = os.listdir(prm.network_shared_path)
+            #log.debug('shared dir list: ' + str(ndirlist))
+            hosts_ready = True
+            if os.path.exists(abortfn):
+                raise Exception('worker host signaled abort')
+            for j in range(last_host_seen + 1, len(prm.host_set)):
+                h = prm.host_set[j]
+                fn = multi_thread_workload.gen_host_ready_fname(h.strip())
+                log.debug('checking for host filename ' + fn)
+                if not os.path.exists(fn):
+                    hosts_ready = False
+                    break
+                last_host_seen = j  # saw this host's ready file
+                # we exit while loop only if no hosts in host_timeout seconds
+                sec = 0.0
+            if hosts_ready:
+                break
+
+            # if one of ssh threads has died, no reason to continue
+
+            kill_remaining_threads = False
+            for t in remote_thread_list:
+                if not t.isAlive():
+                    log.error('thread %s has died' % t)
+                    kill_remaining_threads = True
+                    break
+            if kill_remaining_threads:
+                break
+
+            # be patient for large tests
+            # give user some feedback about
+            # how many hosts have arrived at the starting gate
+
+            time.sleep(sec_delta)
+            sec += sec_delta
+            sec_delta += 1
+            log.debug('last_host_seen=%d sec=%d' % (last_host_seen, sec))
     except KeyboardInterrupt as e:
-        print("received SIGINT (control-C) signal, aborting...")
-        break
-    if rc != OK:
-        print("%s returns %d" % (name, rc))
-        total_errors += 1
-    if (params.stats_report_interval > 0) and (before - last_stat_time > params.stats_report_interval):
-        if params.short_stats == True:
-            output_results.print_short_stats(start_time)
+        log.error('saw SIGINT signal, aborting test')
+        exception_seen = e
+    except Exception as e:
+        exception_seen = e
+        hosts_ready = False
+    if not hosts_ready:
+        multi_thread_workload.abort_test(prm.abort_path, remote_thread_list)
+        if not exception_seen:
+            raise FsDriftException('hosts did not reach starting gate ' +
+                            'within %d seconds' % host_timeout)
         else:
-            output_results.print_stats(start_time, total_errors)
-        last_stat_time = before
-    if (params.drift_time > 0) and (before_drift - last_drift_time > params.drift_time):
-        fsop.simulated_time += params.drift_time
-        last_drift_time = before_drift
+            log.exception(e)
+            raise e
+    else:
 
-if params.rsptimes:
-    for key, ls in list(rsptimes.items()):
-        rsptime_file.write(key+'\n')
-        for (reltime, rspt) in ls:
-            rsptime_file.write('%9.3f , %9.6f\n' % (reltime,  rspt))
-    rsptime_file.close()
-    print('response time file is %s' % rsptime_filename)
+        # ask all hosts to start the test
+        # this is like firing the gun at the track meet
 
-output_results.print_stats(start_time, total_errors)
-if params.starting_gun_file:
-    common.ensure_deleted(params.starting_gun_file)
-common.ensure_deleted(stop_file)
+        try:
+            sync_files.write_sync_file(prm.starting_gun_path, 'hi')
+            log.debug('starting all threads by creating starting gun file %s' %
+                        prm.starting_gun_path)
+        except IOError as e:
+            log.error('error writing starting gun file: %s' % os.strerror(e.errno))
+            multi_thread_workload.abort_test(prm.abort_path, remote_thread_list)
+            raise e
+            
+    # wait for them to finish
+
+    for t in remote_thread_list:
+        t.join()
+        if t.status != OK:
+            log.error('ssh thread for host %s completed with status %d' %
+                      (t.remote_host, t.status))
+
+    # attempt to aggregate results by reading pickle files
+    # containing SmallfileWorkload instances
+    # with counters and times that we need
+
+    try:
+        all_ok = NOTOK
+        invoke_list = []
+        one_shot_delay = True
+        for h in prm.host_set:  # for each host in test
+
+            # read results for each thread run in that host
+            # from python pickle of the list of SmallfileWorkload objects
+
+            pickle_fn = multi_thread_workload.host_result_filename(h)
+            log.debug('reading pickle file: %s' % pickle_fn)
+            host_invoke_list = []
+            try:
+                if one_shot_delay and not os.path.exists(pickle_fn):
+
+                    # all threads have joined already, they are done
+                    # we allow > 1 sec
+                    # for this (NFS) client to see other clients' files
+
+                    time.sleep(1.2)
+                    one_shot_delay = False
+                host_invoke_list = read_pickle(pickle_fn)
+                log.debug(' read %d invoke objects' % len(host_invoke_list))
+                invoke_list.extend(host_invoke_list)
+                ensure_deleted(pickle_fn)
+            except IOError as e:
+                if e.errno != errno.ENOENT:
+                    raise e
+                log.error('  pickle file %s not found' % pickle_fn)
+
+        output_results.output_results(prm, invoke_list)
+        all_ok = OK
+    except IOError as e:
+        log.exception(e)
+        log.error('host %s filename %s: %s' % (h, pickle_fn, str(e)))
+    except KeyboardInterrupt as e:
+        log.error('control-C signal seen (SIGINT)')
+    except FsDriftException as e:
+        log.exception(e)
+
+    sys.exit(all_ok)
+
+
+# main routine that does everything for this workload
+
+def run_workload():
+
+    log = fsd_log.start_log('fs-drift')
+
+    # if a --host-set parameter was passed,
+    # it's a multi-host workload
+    # each remote instance will wait
+    # until all instances have reached starting gate
+
+    try:
+        params = opts.parseopts()
+    except FsDriftException as e:
+        log.error(str(e))
+        log.info('use --help option to get CLI syntax')
+        sys.exit(NOTOK)
+
+    print(params)
+
+    if os.getenv('DEBUG'):
+        log.logLevel(logging.DEBUG)
+
+    try:
+        sync_files.create_top_dirs(params)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise FsDriftException(
+                'you must create the top-level directory %s' % 
+                params.top_directory)
+
+    # put parameters where all threads can see them
+
+    write_pickle(params.param_pickle_path, params)
+
+    if params.host_set != [] and not params.is_slave:
+        return run_multi_host_workload(params, log)
+    return multi_thread_workload.run_multi_thread_workload(params)
+
+
+# for future windows compatibility,
+# all global code (not contained in a class or subroutine)
+# must be moved to within a routine unless it's trivial (like constants)
+# because windows doesn't support fork().
+
+if __name__ == '__main__':
+    run_workload()
