3246006d4c0bd5c197f3213c5ca1c5e3be12a8b0
Ben England <bengland@redhat.com>
2018-12-03 16:44:32 -0500

    add read_pickle()

--- sync_files.py
+++ sync_files.py
@@ -13,13 +13,15 @@ def write_sync_file(fpath, contents):
         sgf.flush()
         os.fsync(sgf.fileno())  # file should close when you exit with block
 
-
 def write_pickle(fpath, obj):
     with open(fpath, 'wb') as result_file:
         pickle.dump(obj, result_file)
         result_file.flush()
         os.fsync(result_file.fileno())  # or else reader may not see data
 
+def read_pickle(fpath):
+    with open(fpath, 'rb') as result_file:
+        return pickle.load(result_file)
 
 def create_top_dirs(prm):
     is_multi_host = (prm.host_set != [])
