923d28a6741d0d6cf175dab6c9a467b97a2c81cb
Ben England <bengland@redhat.com>
2018-12-17 17:12:43 -0500

    allow verbosity to be changed dynamically
    we have to allow FileHandler loglevel to be adjusted between
    logging.INFO and logging.DEBUG on the fly to allow this.

--- fsd_log.py
+++ fsd_log.py
@@ -24,10 +24,24 @@ def start_log(prefix, verbosity=0):
     log.info('starting log')
     return log
 
+# assumptions:
+# - there is only 1 FileHandler associated with logger
+# - you don't want to change loglevel of StreamHandler
+
+def change_loglevel(logger, loglevel):
+    for h in logger.handlers:
+        if isinstance(h, logging.FileHandler):
+            logger.info('changing log level of FileHandler to %s' % loglevel)
+            h.setLevel(loglevel)
+
 if __name__ == '__main__':
     log = start_log('fsd_log_test')
     log.error('level %s', 'error')
     log.warn('level %s', 'warn')
     log.info('level %s', 'info')
     log.debug('level %s', 'debug')
+    change_loglevel(log, logging.DEBUG)
+    log.debug('level %s', 'debug - should see this one in the log file /var/tmp/fsd.fsd_log_test.log')
+    change_loglevel(log, logging.INFO)
+    log.debug('level %s', 'debug - should NOT see this one there')
 
