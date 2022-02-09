d5e73bcda38b3e25ceb693eee31b00dd4ff6d1b3
Samuel Petrovic <spetrovi@redhat.com>
2022-02-09 15:46:09 +0100

    Adding try_to_close to exception handling
    Signed-off-by: Samuel Petrovic <spetrovi@redhat.com>

--- fsop.py
+++ fsop.py
@@ -312,6 +312,7 @@ class FSOPCtx:
                 total_read += count
             c.have_read += 1
         except OSError as e:
+            self.try_to_close(fd, fn)
             if e.errno == errno.ENOENT:
                 c.e_file_not_found += 1
             elif e.errno == errno.ESTALE and self.params.tolerate_stale_fh:
@@ -365,6 +366,7 @@ class FSOPCtx:
                 c.randread_requests += 1
             c.have_randomly_read += 1
         except OSError as e:
+            self.try_to_close(fd, fn)
             if e.errno == errno.ENOENT:
                 c.e_file_not_found += 1
             elif e.errno == errno.ESTALE and self.params.tolerate_stale_fh:
@@ -429,6 +431,7 @@ class FSOPCtx:
             rc = self.maybe_fsync(fd)
             c.have_created += 1
         except OSError as e:
+            self.try_to_close(fd, fn)        
             if e.errno == errno.EEXIST:
                 c.e_already_exists += 1
             elif e.errno == errno.ENOSPC or e.errno == errno.EDQUOT:
@@ -476,6 +479,7 @@ class FSOPCtx:
             rc = self.maybe_fsync(fd)
             c.have_appended += 1
         except OSError as e:
+            self.try_to_close(fd, fn)        
             if e.errno == errno.ENOENT:
                 c.e_file_not_found += 1
             elif e.errno == errno.ENOSPC or e.errno == errno.EDQUOT:
@@ -524,6 +528,7 @@ class FSOPCtx:
                 rc = self.maybe_fsync(fd)
             c.have_randomly_written += 1
         except OSError as e:
+            self.try_to_close(fd, fn)        
             if e.errno == errno.ENOENT:
                 c.e_file_not_found += 1
             elif e.errno == errno.ENOSPC or e.errno == errno.EDQUOT:
@@ -550,6 +555,7 @@ class FSOPCtx:
             os.ftruncate(fd, new_file_size)
             c.have_truncated += 1
         except OSError as e:
+            self.try_to_close(fd, fn)        
             if e.errno == errno.ENOENT:
                 c.e_file_not_found += 1
             elif e.errno == errno.ENOSPC or e.errno == errno.EDQUOT:
