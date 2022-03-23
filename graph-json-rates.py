92e98ac7e0fa7dba4b03842015322f1d818982b6
Ben England <bengland@redhat.com>
2022-03-23 08:34:19 -0400

    add parameter for rate measurement time interval
    adjust x-axis label to be accurate
    move legend to the right of data

--- graph-json-rates.py
+++ graph-json-rates.py
@@ -3,12 +3,13 @@
 # input parameters:
 #   1: .json file produced by fs-drift and compute_rates.py
 #   2: counter type : fps (for files-per-second) or MBps (for MiB/sec)
-#   3: optional entity to graph
+#   3: time interval covered by each set of rates (sec)
+#   4: [optional] entity to graph
 # some .json files contain data for multiple threads or multiple hosts
 # in these cases you have to choose one, and it will help you by listing possibilities
 # so for example:
-#   $ graph-json-rates.py /mnt/cephfs/network-shared/cluster-rates.json MBps
-#   $ graph-json-rates.py /mnt/cephfs/network-shared/per-host-rates.json fps localhost.localdomain.00
+#   $ graph-json-rates.py /mnt/cephfs/network-shared/cluster-rates.json MBps 5
+#   $ graph-json-rates.py /mnt/cephfs/network-shared/per-host-rates.json fps 5 localhost.localdomain.00 
 # it generates a .png file containing the desired graph with a unique filename 
 # in the same directory as the .json file that generated it
 # if you don't want to see the graph and you just want .png file, unset DISPLAY env. var.
@@ -22,16 +23,21 @@ import json
 
 def usage(errmsg):
     print('ERROR: %s' % errmsg)
-    print('usage: graph-json-rates.py fs-drift-result-json-file fps|mbps [ optional-key ] ')
+    print('usage: graph-json-rates.py fs-drift-rates-json-file fps|mbps interval-sec [ optional-key ] ')
     sys.exit(1)
 
 if len(argv) < 3:
     usage('not enough CLI parameters')
 input_json_path = argv[1]
 want_fps = (argv[2] == 'fps')
+try:
+    interval_sec = float(argv[3])
+except ValueError:
+    usage('could not parse time interval %s' % str(argv[2]))
+
 select_key = None
-if len(argv) == 4:
-    select_key = argv[3]
+if len(argv) == 5:
+    select_key = argv[4]
 with open(input_json_path) as f:
   data = json.load(f)
 print('JSON result file: %s' % input_json_path)
@@ -54,6 +60,9 @@ else:
 key_source = selected_entity_rates[0].keys()
 for k in key_source:
     vars[k] = []
+rate_intervals = len(selected_entity_rates)
+elapsed_time = rate_intervals * interval_sec
+print('elapsed time of test run: %f' % interval_sec)
 
 # separate out the files-per-sec and bytes-per-sec values
 
@@ -74,6 +83,7 @@ for k in d.keys():
         files_data[k] = vars[k]
         max_value = max(max_value, max(vars[k]))
         sample_count = len(vars[k])
+
 
 # call pyplot to generate the graph
 
@@ -96,9 +106,9 @@ else:
     for k in bytes_data.keys():
         bytes_lines.append(ax.plot(range(0,sample_count), bytes_data[k], label=k))
 
-ax.legend(loc='upper right')
+ax.legend(loc='upper right', fontsize='medium',bbox_to_anchor=(1.1,1.0))
 ax.set_ylim([0.0, max_value])
-plt.xlabel('sample (time)', fontdict=font)
+plt.xlabel('fraction of elapsed time', fontdict=font)
 plt.ylabel('files/sec' if want_fps else 'MiB/sec', fontdict=font)
 title = 'fs-drift rates'
 if select_key is not None:
