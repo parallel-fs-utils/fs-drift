c22b2470af70d6feac0a5399fea8607a80066504
Jan Jurca <jjurcaj@gmail.com>
2022-07-26 12:32:56 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="fs-drift",
+    version="v0.2",
+    description="Scripts and libs for Beaker XML FS generation.",
+    long_description=long_description,
+    url="https://github.com/parallel-fs-utils/fs-drift",
+    keywords=[],
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=[
+        "pyaml",
+        "numpy"
+    ],
+    entry_points={
+        'console_scripts': [
+            'fs-drift = fs-drift:__main__',
+        ]
+    }
+)
